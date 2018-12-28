aecda9fbaefc60fdc3f8543fc3e89ca249f7754f
Marco Bayer <spock_e@gmx.de>
2018-12-28 10:01:13 +0100

    added debug tools for ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { environment } from './../environments/environment';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,17 +11,22 @@ import { AppComponent } from './app.component';
 import { PasswordListComponent } from './password-list/password-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PasswordListComponent
-  ],
+  declarations: [AppComponent, PasswordListComponent],
   imports: [
     BrowserModule,
     FlexLayoutModule,
     BrowserAnimationsModule,
-    NgxsModule.forRoot()
+    NgxsModule.forRoot([], {
+      developmentMode: !environment.production
+    }),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production
+    }),
+    NgxsReduxDevtoolsPluginModule.forRoot({
+      disabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
