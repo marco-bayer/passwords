ae9ffd72407507eeffb15bd1a108b5a5ddb4d1f4
Marco Bayer <spock_e@gmx.de>
2018-12-28 09:51:11 +0100

    new component password-list and ngxs installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { NgxsModule } from '@ngxs/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PasswordListComponent } from './password-list/password-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordListComponent
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgxsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
